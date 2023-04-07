d678d190bb9f461f3e66483571aa21bae9c506b7
ThisProgrammerG <116992225+ThisProgrammerG@users.noreply.github.com>
2023-04-07 11:24:12 -0500

    Fix for when there is no firefox window open

--- window_handler.py
+++ window_handler.py
@@ -1,12 +1,26 @@
 import subprocess
 
+import win32process
+
+from window import Window
+
 
 class WindowHandler:
-    def __init__(self, window):
+    def __init__(self, window: Window):
         self.window = window
 
     def run(self):
-        subprocess.run([self.window.program_path])
+        win32process.CreateProcess(
+                str(self.window.program_path),  # ApplicationName (None means use command line)
+                None,  # CommandLine
+                None,  # ProcessAttributes (None means inherit from parent process)
+                None,  # ThreadAttributes (None means inherit from parent process)
+                False,  # bInheritHandles (False means don't inherit any handles)
+                0,  # dwCreationFlags (0 means create process normally)
+                None,  # NewEnvironment (None means use parent process's environment)
+                None,  # CurrentDirectory (optional)
+                win32process.STARTUPINFO()  # StartupInfo
+        )
 
     def move_window(self, primary_monitor: bool) -> None:
         """ Moves window to the right if not primary monitor. """
